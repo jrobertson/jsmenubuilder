12fa4e934ba33eaa9170a19d48d5459e0507a2a2
James Robertson <james@jamesrobertson.eu>
2019-07-10 23:30:54 +0100

    1st commit

---
+++ jsmenubuilder.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'jsmenubuilder'
+  s.version = '0.1.0'
+  s.summary = 'Generates HTML based tabs using HTML, CSS, and JavaScript.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/jsmenubuilder.rb']
+  s.add_runtime_dependency('rexle', '~> 1.5', '>=1.5.1')
+  s.signing_key = '../privatekeys/jsmenubuilder.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@jamesrobertson.eu'
+  s.homepage = 'https://github.com/jrobertson/jsmenubuilder'
+end
---
+++ lib/jsmenubuilder.rb
@@ -0,0 +1,159 @@
+#!/usr/bin/env ruby
+
+# file: jsmenubuilder.rb
+
+require 'rexle'
+require 'rexle-builder'
+
+class JsMenuBuilder
+
+TABS_CSS =<<EOF
+/* Style the tab */
+.tab {
+  overflow: hidden;
+  border: 1px solid #ccc;
+  background-color: #f1f1f1;
+}
+
+/* Style the buttons that are used to open the tab content */
+.tab button {
+  background-color: inherit;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 14px 16px;
+  transition: 0.3s;
+}
+
+/* Change background color of buttons on hover */
+.tab button:hover {
+  background-color: #ddd;
+}
+
+/* Create an active/current tablink class */
+.tab button.active {
+  background-color: #ccc;
+}
+
+/* Style the tab content */
+.tabcontent {
+  display: none;
+  padding: 6px 12px;
+  border: 1px solid #ccc;
+  border-top: none;
+}
+EOF
+
+TABS_JS =<<EOF
+function openTab(evt, tabName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+EOF
+
+
+
+  attr_reader :html, :css, :js
+
+  def initialize(type, options={})
+
+    @type = type
+    types = %i(tabs)
+    method(type.to_sym).call(options) if types.include? type
+
+  end
+  
+  def to_css()
+    @css
+  end
+  
+  def to_html()
+    @html
+  end
+  
+  def to_js()
+    @js
+  end
+  
+  def to_webpage()
+
+    a = RexleBuilder.build do |xml|
+      xml.html do 
+        xml.head do
+          xml.meta name: "viewport", content: \
+              "width=device-width, initial-scale=1"
+          xml.style "\nbody {font-family: Arial;}\n\n" + @css
+        end
+        xml.body
+      end
+    end
+
+    doc = Rexle.new(a)
+    e = Rexle.new("<html>%s</html>" % @html).root
+    
+    e.children.each {|child| doc.root.element('body').add child }
+    
+    doc.root.element('body').add \
+        Rexle::Element.new('script').add_text "\n" + 
+        @js.gsub(/^ +\/\/[^\n]+\n/,'')
+    
+    "<!DOCTYPE html>\n" + doc.xml(pretty: true, declaration: false)\
+        .gsub(/<\/div>/,'\0' + "\n").gsub(/\n *<!--[^>]+>/,'')
+    
+  end
+  
+  private
+
+  def tabs(options={})
+
+    ## build the HTML
+
+    a = RexleBuilder.build do |xml|
+      xml.html do 
+
+        xml.comment!(' Tab links ')
+        xml.div(class: 'tab' ) do
+          options[:headings].each do |heading|
+            xml.button({class:'tablinks', 
+                        onclick: %Q(openTab(event, "#{heading}"))}, heading)
+          end
+        end
+
+        xml.comment!(' Tab content ')
+
+        options[:headings].each do |heading|
+          xml.div(id: heading, class: 'tabcontent' ) do
+            xml.h3 heading
+          end
+        end
+      end
+    end
+
+    @html = Rexle.new(a).xml(pretty: true, declaration: false)\
+      .gsub(/<\/div>/,'\0' + "\n").strip.lines[1..-2]\
+      .map {|x| x.sub(/^  /,'') }.join
+    
+    @css = Object.const_get 'JsMenuBuilder::' + @type.to_s.upcase + '_CSS'
+    @js = Object.const_get 'JsMenuBuilder::' + @type.to_s.upcase + '_JS'
+    
+  end
+
+end
